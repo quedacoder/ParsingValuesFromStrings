c9e8a2b5256855b4597ae29126fb7a084ad2ee9e
quedacoder <quedacoder@gmail.com>
2022-03-25 15:46:26 -0500

    Examples parsing int and double from a string

--- src/com/quedacoder/Main.java
+++ src/com/quedacoder/Main.java
@@ -3,6 +3,19 @@ package com.quedacoder;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String numberAsString = "2018a";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int numberFromString = Integer.parseInt(numberAsString);
+        System.out.println("numberAsInteger = " + numberFromString);
+
+        numberAsString += 1;
+        numberFromString += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("numberAsInteger = " + numberFromString);
+
+        double doubleNumber = Double.parseDouble("123.45");
+        System.out.println("doubleNumber = " + doubleNumber);
     }
 }
